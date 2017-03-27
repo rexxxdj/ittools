76e547fdb058e636ddfa3976c09bf515a78dd416
Ruslan Tokar <djrexxx87@gmail.com>
2017-03-27 16:38:01 +0200

    add dynamic team & services

--- services/views.py
+++ services/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
-def main_page(request):
-    return render(request,'services/index.html',{})
-
 def service_list(request):
-    return render(request,'services/services.html',{})
+    services = (
+        {'id': 1,
+         'name': 'Project Name',
+         'description': 'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.',
+         'image': 'img/desk.jpg'            
+        },
+        {'id': 2,
+         'name': 'Project Name',
+         'description': 'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.',
+         'image': 'img/building.jpg'            
+        },
+        {'id': 3,
+         'name': 'Project Name',
+         'description': 'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.',
+         'image': 'img/loft.jpg'            
+        },
+        {'id': 4,
+         'name': 'Project Name',
+         'description': 'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, interdum justo suscipit id.',
+         'image': 'img/minibus.jpeg'            
+        }
+    )
+    return render(request,'services/services.html',{'services': services})
--- team/views.py
+++ team/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def team_list(request):
-    return render(request,'team/team.html',{})
+    team = (
+        {'id': 1,
+         'first_name': u'Ben',
+         'last_name': u'Johnson',
+         'position': u'Musician',
+         'description': u'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.',         
+         'image':'img/1.jpg'},
+        {'id': 2,
+         'first_name': u'Emily',
+         'last_name': u'Clark',
+         'position': u'Artist',
+         'description': u'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.',         
+         'image':'img/2.jpg'},
+        {'id': 3,
+         'first_name': u'Carl',
+         'last_name': u'Kent',
+         'position': u'Stylist',
+         'description': u'Aenean tortor est, vulputate quis leo in, vehicula rhoncus lacus. Praesent aliquam in tellus eu gravida. Aliquam varius finibus est, et interdum justo suscipit id. Etiam dictum feugiat tellus, a semper massa.',         
+         'image':'img/3.jpg'}
+    )
+    return render(request,'team/team.html',{'team': team})
