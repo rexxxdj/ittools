1162f2053515f8b8053ada3a4368514cb82793e6
Ruslan Tokar <djrexxx87@gmail.com>
2017-04-25 14:49:47 +0200

    redirect

--- services/urls.py
+++ services/urls.py
@@ -6,6 +6,5 @@ from services.views import journal
 
 urlpatterns = [
     url(r'^$',services.service_list,name='service'),
-    url(r'^(?P<sid>\d+)/$',services.service_unit,name='service unit'),
     url(r'^journal/$',journal.JournalView.as_view(),name='journal')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
